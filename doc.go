60bd464893e5619fbccb703018c6ed554a7997d4
Christian Neumann <cneumann@datenkarussell.de>
2012-12-02 13:39:18 +0100

    Initial commit.

---
+++ doc.go
@@ -0,0 +1,20 @@
+// This file is part of monsti/form.
+// Copyright 2012 Christian Neumann
+
+// monsti/form is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+
+// monsti/form is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+
+// You should have received a copy of the GNU Lesser General Public License
+// along with monsti/form. If not, see <http://www.gnu.org/licenses/>.
+
+/*
+Package monsti/form implements a web form generator and validator.
+*/
+package form
